b41e9e2a93d7b815854e3eab77a77df36c24b92d
Youtpout <EddyBoughioul@hotmail.fr>
2024-03-25 22:28:03 +0100

    call works locally

--- app/frames/send/route.tsx
+++ app/frames/send/route.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-key */
 import { NextRequest, NextResponse } from "next/server";
 import { TransactionTargetResponse, getFrameMessage } from "frames.js";
-import { parseEther } from 'viem';
+import { parseEther, encodeFunctionData } from 'viem';
 import { checkQuery } from "../../utils";
 
 export async function POST(
@@ -39,11 +39,14 @@ export async function POST(
     const amount = parseEther(frameMessage.inputText!);
 
     const abi = [{ "anonymous": false, "inputs": [{ "indexed": false, "internalType": "uint8", "name": "version", "type": "uint8" }], "name": "Initialized", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "previousOwner", "type": "address" }, { "indexed": true, "internalType": "address", "name": "newOwner", "type": "address" }], "name": "OwnershipTransferred", "type": "event" }, { "inputs": [], "name": "FEE_DESTINATION", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "SHORTCUT_BASE_FEE", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "SHORTCUT_COMPLEXITY", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "enum PoolType", "name": "_pType", "type": "uint8" }, { "internalType": "address", "name": "_pool", "type": "address" }, { "internalType": "address", "name": "_swapRouter", "type": "address" }], "name": "initialize", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "originWeth", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "owner", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "pool", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "renounceOwnership", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "swapRouter", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "newOwner", "type": "address" }], "name": "transferOwnership", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "token", "type": "address" }, { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "withdrawTokens", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "stateMutability": "payable", "type": "receive" }];
+
+
+
     const tx = {
-        abi,
         chainId: "eip155:" + idChain, // OP Mainnet 10
         method: "eth_sendTransaction",
         params: {
+            abi,
             to: address,
             value: amount.toString()
         },
